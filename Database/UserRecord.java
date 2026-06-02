2060e35fdb9d3010c1a6aa342f29ea1a3609e509
MUHAMMAD NAQIUDDIN <24001097@siswa.um.edu.my>
2026-06-02 17:47:13 +0800

    Add files via upload

---
+++ Database/UserRecord.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+/**
+ *
+ * @author Naqiuddin
+ */
+public class UserRecord {
+    
+    private String userID;
+    private String lastOrderDate;
+    
+    public UserRecord (String userID, String lastOrderDate) {
+        this.userID = userID;
+        this.lastOrderDate = lastOrderDate;
+    }    
+}
